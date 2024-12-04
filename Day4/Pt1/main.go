c67050b0df6349c41f573873b844af60de133b09
Van <142906115+VanAdcox@users.noreply.github.com>
2024-12-04 19:41:37 +0000

    start on a day 4 pt 1 implementation

--- Day4/Pt1/main.go
+++ Day4/Pt1/main.go
@@ -1,8 +1,45 @@
 package main
 
 import (
+	"fmt"
+	"strings"
 )
 
-
 func main() {
+	input := `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+`
+	originalCrossword := strings.Split(input, "\n")
+	/*
+		scanner := bufio.NewScanner(strings.NewReader(input))
+		for scanner.Scan() {
+
+		}
+	*/
+
+	total := 0
+	crossword := make([]string, len(originalCrossword))
+	copy(crossword, originalCrossword)
+
+	for y := 0; y < len(originalCrossword); y++ {
+		for x := 0; x < len(originalCrossword[y]); x++ {
+			newCrossword, wordFound := wasWordFound(crossword, string(crossword[y][x]), y, x)
+			if wordFound {
+				total += 1
+			}
+			crossword = newCrossword
+		}
+	}
+	fmt.Println(total)
+}
+func wasWordFound(crossword []string, curLetter string, y, x int) ([]string, bool) {
+	
 }
